33d86d11d8d80ec024dda151126f28ec41449913
bhaskardavuluri <bhaskardavuluri9949@gmail.com>
2025-05-22 22:57:10 +0530

    DATATYPES

---
+++ Class-3 DATATYPES/Test.java
@@ -0,0 +1,21 @@
+public class Test{
+  
+    public static void main(String[] args) {
+        byte b=127;
+        char  grade='A';
+        int a=30;
+        boolean discount=true;
+        float salary=45000.500f;
+        String message="HELLO";
+        long empsal=1223456671;
+        System.out.println(a);
+        System.out.println(message);
+        System.out.println(discount);
+        System.out.println(salary);
+        System.out.println(empsal);
+
+        System.out.println(b);
+        System.out.println(grade);
+    }
+
+}
