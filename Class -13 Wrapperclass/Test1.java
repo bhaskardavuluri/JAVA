1593cbe1a6915f276971de72711e131f020e130c
bhaskardavuluri <bhaskardavuluri9949@gmail.com>
2025-06-27 11:36:14 +0530

    All

---
+++ Class -13 Wrapperclass/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String[] args) {
+        int a=100;
+        Integer b=a;//(autoboxing)
+        Integer c=Integer.valueOf(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ Class -13 Wrapperclass/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+    public static void main(String[] args) {
+        int a=100;
+        Integer b=a;//(autoboxing prirmitive data type into the object )
+        int c=b;   //(unboxing the object into primitive data type)
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
